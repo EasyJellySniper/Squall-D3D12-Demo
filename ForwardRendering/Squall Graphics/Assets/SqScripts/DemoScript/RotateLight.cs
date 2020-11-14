c8e5a80a456cfbadaf02651724db474265aaa2bd
Chieh Hung Liu (Squall Liu) <ltes50414@gmail.com>
2020-11-14 22:26:23 +0800

    Rotate light

---
+++ ForwardRendering/Squall Graphics/Assets/SqScripts/DemoScript/RotateLight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// simple rotate light
+/// </summary>
+public class RotateLight : MonoBehaviour
+{
+    /// <summary>
+    /// rotate speed
+    /// </summary>
+    public float rotateSpeed = 10f;
+
+    /// <summary>
+    /// rotate axis
+    /// </summary>
+    public Vector3 rotateAxis = Vector3.up;
+
+    void Update()
+    {
+        transform.Rotate(rotateAxis, rotateSpeed * Time.deltaTime, Space.World);
+    }
+}
